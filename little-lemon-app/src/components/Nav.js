5219df66243e0cb74411cc1922201e33af48cafb
Daniel <danielttgb44@live.com.mx>
2023-02-25 11:44:02 -0600

    Added semantic HTML tags

---
+++ little-lemon-app/src/components/Nav.js
@@ -0,0 +1,28 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+        <li>
+          <a href="index.html">About</a>
+        </li>
+        <li>
+          <a href="index.html">Menu</a>
+        </li>
+        <li>
+          <a href="index.html">Reservations</a>
+        </li>
+        <li>
+          <a href="index.html">Order Online</a>
+        </li>
+        <li>
+          <a href="index.html">Log In</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
